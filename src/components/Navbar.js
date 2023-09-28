0eb04862a9f55acced93eacee8ed216ec0f19fdf
bhushan354 <deshmukhbhushan354@gmail.com>
2023-09-28 08:40:24 +0530

    use reducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,17 @@
 import { CartIcon } from "../icons";
 import { useSelector as UseSelector } from 'react-redux';
-import { store } from "../store";
 
 const Navbar = () => {
-    console.log(UseSelector((store) => {
-        console.log(store); 
-    }));
+  const amount = UseSelector((store) => store.cart.amount);
   return (
     <nav>
       <div className="nav-center">
         <h3>redux toolkit</h3>
         <div className="nav-container">
-            <CartIcon />
-            <div className="amount-container">
-                <p className="total-amount">0</p>
-            </div>
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
         </div>
       </div>
     </nav>
